53a4a22ef590481132ea74ab65c2c54d08b5c2cd
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-29 12:35:38 +0100

    Dodaj pliki projektów.

---
+++ StackOverflowTags.Domain/Entities/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace StackOverflowTags.Domain.Entities
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = default!;
+        public int Count {  get; set; }
+        public double Percentage { get; set; }
+
+    }
+}
