73611ca01b7587c3bfdce8ec70289e77f078863a
Jakub Wabik <40063902+DeiranZ@users.noreply.github.com>
2024-03-29 13:38:42 +0100

    Database setup

---
+++ SqlScripts/setup.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `StackOverflowTagsDb.Tags` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `count` int DEFAULT NULL,
+  `percentage` real DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1;
--- StackOverflowTags.Domain/Entities/Tag.cs
+++ StackOverflowTags.Domain/Entities/Tag.cs
@@ -1,12 +1,14 @@
-﻿namespace StackOverflowTags.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StackOverflowTags.Domain.Entities
 {
+    [Table("Tags")]
     public class Tag
     {
         public int Id { get; set; }
 
         public string Name { get; set; } = default!;
         public int Count {  get; set; }
-        public double Percentage { get; set; }
-
+        public float Percentage { get; set; }
     }
 }
